2909858910618949f16e0d6f5403b9684eba7ae0
Kevin Beaton <kabton14@gmail.com>
2023-03-24 22:16:41 -0500

    Add bp middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bp = require('body-parser');
+
 const app = express();
 
 // Basic Configuration
@@ -10,6 +11,11 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,6 +24,10 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', (req, res) => {
+   res.json({ original_url : req.params.short_url, short_url : 1})
 });
 
 app.listen(port, function() {
